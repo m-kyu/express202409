1fd6e9282b95d601cceeade71a8ecebba43cadbd
chase <yicha7@gmail.com>
2024-09-06 18:42:47 +0900

    sdfsdf

--- api/todos.js
+++ api/todos.js
@@ -2,8 +2,12 @@ const express = require('express');
 const fs = require('fs');
 const todos = express.Router()
 
+let data = fs.readFileSync('./db/data.json');
+let dataParse = JSON.parse(data);
+
 todos.get('/', function (req, res) {
-  res.send( 'Hello' )
+  res.send( dataParse )
 })
+
 
 module.exports = todos;

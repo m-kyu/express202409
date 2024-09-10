784e36b2e81fd8e61e2d5686955a9d8efcdfa08c
chase <yicha7@gmail.com>
2024-09-10 14:59:33 +0900

    google

--- api/todos.js
+++ api/todos.js
@@ -3,7 +3,8 @@ const todos = express.Router()
 const { MongoClient } = require('mongodb');
 
 const dbName = 'todos';
-const url = 'mongodb+srv://minkyu:abcdef2397@minkyu.rlol7cf.mongodb.net/?retryWrites=true&w=majority&appName=minkyu';
+// const url = 'mongodb+srv://minkyu:abcdef2397@minkyu.rlol7cf.mongodb.net/?retryWrites=true&w=majority&appName=minkyu';
+const url = 'mongodb+srv://yicha7:test1234@cluster2.kjcecrw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster2';
 const client = new MongoClient(url);
 
 async function connect(){
@@ -33,29 +34,26 @@ todos.get('/:id', async function (req, res) {
 todos.post('/', async function (req, res) {
     const collection = await connect();   
                        await collection.insertOne(req.body);
-    // const findResult = await collection.find({}).toArray();
     client.close();
-    res.send(req.body)
+    res.send('done');
 })
 
 
 todos.put('/', async function (req, res) {
+    console.log(req.body)
     const collection = await connect();   
                        await collection.updateOne({id:req.body.id},{$set:req.body});
-    const findResult = await collection.find({}).toArray();
     client.close();
 
-    res.send(findResult)
+    res.send('done');
 })
 
 todos.delete('/', async function (req, res) {
 
     const collection = await connect();   
                        await collection.deleteOne(req.query);
-    const findResult = await collection.find({}).toArray();
     client.close();
-
-    res.send(findResult)
+    res.send('done');
 })
 
 module.exports = todos;
